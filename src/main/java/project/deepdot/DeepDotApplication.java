05293c6c52006cec654af57fa89acee947aed817
bomikim0214 <rlaqhals05@naver.com>
2025-07-19 16:05:21 +0900

    초기 설정

---
+++ src/main/java/project/deepdot/DeepDotApplication.java
@@ -0,0 +1,13 @@
+package project.deepdot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepDotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepDotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/deepdot/DeepDotApplicationTests.java
@@ -0,0 +1,13 @@
+package project.deepdot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepDotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
