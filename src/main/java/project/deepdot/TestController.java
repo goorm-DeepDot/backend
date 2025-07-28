126e6920fd8f1c48d9bac97c7498a51b3a83b834
bomikim0214 <rlaqhals05@naver.com>
2025-07-29 00:48:49 +0900

    [Test#9] ci/cd test

---
+++ src/main/java/project/deepdot/TestController.java
@@ -0,0 +1,12 @@
+package project.deepdot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public void test(){
+        System.out.println("test 성공");
+    }
+}
